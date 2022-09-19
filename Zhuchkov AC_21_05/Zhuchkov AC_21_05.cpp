69884eeebaa28015fdbf47260d431e25fdb6836c
ZhuchkovAA <112942160+ZhuchkovAA@users.noreply.github.com>
2022-09-19 23:24:20 +0300

    V1.1

--- Zhuchkov AC_21_05/Zhuchkov AC_21_05.cpp
+++ Zhuchkov AC_21_05/Zhuchkov AC_21_05.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <list>
+#include <fstream>
 
 using namespace std;
 
@@ -9,22 +9,24 @@ struct pipe {
     bool _inWork;
 };
 
-pipe yourPipe{};
-void add_pipe() { 
-
-    int length;
-    int diameter;
-    bool inWork;
-    cout << "Введите длину трубы: "; cin >> length;
-    cout << "Введите диаметр трубы: "; cin >> diameter;
-    cout << "В работе: "; cin >> inWork;
-
-    yourPipe = {length, diameter, inWork};
+struct CS {
+    char _name;
+    int _shopCount;
+    int _workShopCount;
+    double _efficiency = double(_workShopCount) / double(_shopCount);
 };
 
-void out_pipe() {
-    cout << "\n\nТруба\nДлина: "<< yourPipe._length <<"\nДиаметр: "<< yourPipe._diameter << "\nВ работе: " << yourPipe._inWork << "\n";
+int isBool(bool num) {
+    if (num == 1 || num == 0) {
+        return 0;
+    }
+    else {
+        return -1;
+    }
 }
+
+pipe yourPipe{};
+CS yourCS{};
 
 int stateMenu;
 void menu() {
@@ -33,52 +35,146 @@ void menu() {
     cin >> stateMenu;
 }
 
+void add_pipe() { 
+
+    int length = 0;
+    int diameter = 0;
+    bool inWork = false;
+
+    do {
+        cout << "Введите длину трубы: "; cin >> length;
+    } while (length <= 0);
+    do {
+        cout << "Введите диаметр трубы: "; cin >> diameter;
+    } while (diameter <= 0);
+    cout << "В работе(1/0): "; cin >> inWork;
+    
+    if (yourPipe._inWork == 204) {
+        length = 0;
+        diameter = 0;
+        inWork = false;
+        system("cls");
+        menu();
+    }
+
+    yourPipe = { length, diameter, inWork };
+};
+
+void out_pipe() {
+    cout << "\n\nТруба\nДлина: "<< yourPipe._length <<"\nДиаметр: "<< yourPipe._diameter << "\nВ работе: " << yourPipe._inWork << "\n";
+}
 
 void red_pipe() {
-    int state;
-    int length;
-    int diameter;
-    bool inWork;
-    cout << "Труба\nРедактировать:\n(1) Длина\n(2) Диаметр\n(3) В работе\n";
-    cin >> state;
-    if (state == 1) {
-        cout << "\nДлина до изменения: " << yourPipe._length << "\nВведите новую длину: ";
-        cin >> length;
-        yourPipe._length = length;
-        system("cls");
-        cout << "\nДанные успешно изменены!\n";
-        system("pause");
-        system("cls");
-        menu();
-    }
-    else if (state == 2) {
-        cout << "\nДиаметр до изменения: " << yourPipe._diameter << "\nВведите новый диаметр: ";
-        cin >> diameter;
-        yourPipe._diameter = diameter;
-        system("cls");
-        cout << "\nДанные успешно изменены!\n";
-        system("pause");
-        system("cls");
-        menu();
-    }
-    else if (state == 3) {
-        cout << "\nДиаметр до изменения: " << yourPipe._inWork << "\nВведите новый диаметр: ";
+    if (yourPipe._length != 0 || yourPipe._diameter != 0) {
+        bool inWork;
+        cout << "\nПараметр 'В работе': " << yourPipe._inWork << "\nВведите новое значение: ";
         cin >> inWork;
-        yourPipe._inWork = inWork;
+        
+        cout << isBool(inWork) << endl << inWork << endl;
+        inWork == false ? yourPipe._inWork = 0 : yourPipe._inWork = 1;
+
         system("cls");
         cout << "\nДанные успешно изменены!\n";
-        system("pause");
-        system("cls");
-        menu();
+        //if (isBool(inWork) == 0) {
+        //    yourPipe._inWork = inWork ;
+        //    system("cls");
+        //    cout << "\nДанные успешно изменены!\n";
+        //}
+        //else {
+        //    system("cls");
+        //    cout << "\nДанные некорректны!\n";
+        //}
     }
-    else{
-        cout << "Некорректные данные..";
-        system("pause");
-        system("cls");
-        menu();
+    else
+    {
+        cout << "\nДобавьте трубу\n\n";
     }
 }
 
+void add_CS() {
+
+    char nameCS[100];
+    int shopCount;
+    int workShopCount;
+
+    cout << "Введите название КС: "; cin >> nameCS;
+    do {
+        cout << "Введите количество цехов: "; cin >> shopCount;
+    } while (shopCount <= 0);
+    do {
+        cout << "Введите количество цехов в работе (Max: " << shopCount << "): "; cin >> workShopCount;
+    } while (shopCount < workShopCount || workShopCount < 0);
+    
+    cout << nameCS;
+
+    yourCS = { nameCS[0], shopCount, workShopCount };
+    
+    cout << "Эффективность: " << yourCS._efficiency;
+};
+
+void out_CS() {
+    cout << "\n\nKC\nНазвание: " << yourCS._name << "\nКоличество цехов: " << yourCS._shopCount << "\nКоличество цехов в работе: " << yourCS._workShopCount << "\nЭффективность: " << yourCS._efficiency << "\n";
+}
+
+void red_CS() {
+    if (yourCS._shopCount != 0 || yourCS._workShopCount != 0) {
+        
+        int workShopCount;
+        
+        cout << "\nПараметр 'Количество цехов в работе': " << yourCS._workShopCount;
+
+        do {
+            cout << "\nВведите новое значение(Max: " << yourCS._shopCount << "): ";  cin >> workShopCount;
+        } while (yourCS._shopCount < workShopCount || workShopCount < 0);
+        
+        
+        yourCS._workShopCount = workShopCount;
+        system("cls");
+        cout << "\nДанные успешно изменены!\n";
+    }
+    else
+    {
+        cout << "\nДобавьте KC\n\n";
+    }
+}
+
+void out_all_to_file() {
+    ofstream fout("out.txt");
+    if (!(yourPipe._length == 0 && yourPipe._diameter == 0 && yourPipe._inWork == 0)) {
+        fout << "Труба\nДлина: " << yourPipe._length << "\nДиаметр: " << yourPipe._diameter << "\nВ работе: " << yourPipe._inWork << "\n\n\n";
+    }
+    else
+    {
+        fout << "Трубы не найдено!\n";
+    }
+    if (!(yourCS._shopCount == 0 && yourCS._workShopCount == 0)) {
+        fout << "KC\nНазвание: " << yourCS._name << "\nКоличество цехов: " << yourCS._shopCount << "\nКоличество цехов в работе: " << yourCS._workShopCount << "\nЭффективность: " << yourCS._efficiency << "\n";
+    }
+    else
+    {
+        fout << "KC не найдено!\n";
+    }
+    fout.close();
+}
+
+void in_all_at_file() {
+    char temp[100];
+    ifstream some_file("in.txt");
+
+    some_file >> temp;
+    cout << temp << endl;
+
+    if (temp == 1) {
+        some_file.getline(temp, 100);
+        yourPipe._length = temp[1];
+        yourPipe._diameter = temp[2];
+        yourPipe._inWork = temp[3];
+    }
+
+    some_file.close();
+
+    cin.get();
+}
 int main()
 {
     menu();
@@ -92,9 +188,20 @@ int main()
             system("cls");
             menu();
             break;
-
+        case 2:
+            add_CS();
+            out_CS();
+            system("pause");
+            system("cls");
+            menu();
+            break;
         case 3:
-            out_pipe();
+            if (!(yourPipe._length == 0 && yourPipe._diameter == 0 && yourPipe._inWork == 0)) {
+                out_pipe();
+            }
+            if (!(yourCS._shopCount == 0 && yourCS._workShopCount == 0)) {
+                out_CS();
+            }
             system("pause");
             system("cls");
             menu();
@@ -102,6 +209,28 @@ int main()
 
         case 4:
             red_pipe();
+            system("pause");
+            system("cls");
+            menu();
+            break;
+
+        case 5:
+            red_CS();
+            system("pause");
+            system("cls");
+            menu();
+            break;
+        case 6:
+            out_all_to_file();
+            system("pause");
+            system("cls");
+            menu();
+            break;
+        case 7:
+            in_all_at_file();
+            system("pause");
+            system("cls");
+            menu();
             break;
         }
     }
